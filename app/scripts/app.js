75eb542731b74ddd54eff2dd4107efcd68e73fb8
Allen Zheng <capken@gmail.com>
2014-10-15 16:23:24 +0800

    setup the project using yeoman

---
+++ app/scripts/app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name airCleanersWebApp
+ * @description
+ * # airCleanersWebApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('airCleanersWebApp', [
+    'ngResource'
+  ]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name airCleanersWebApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the airCleanersWebApp
+ */
+angular.module('airCleanersWebApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
