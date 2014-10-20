ee69fb794e48bf4f1326f85fcfa050ca20fdf022
Allen Zheng <capken@gmail.com>
2014-10-20 14:05:17 +0800

    improve the product review and detail page

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,4 +9,57 @@ angular.module('airCleanersWebApp')
     brand: '布鲁雅尔',
     productId: 1365
   };
+
+  $scope.minVolume = function() {
+    var minAreaStr = $scope.params.area.split(':').shift();
+    var minArea = parseFloat(minAreaStr);
+
+    var minHeightStr = $scope.params.height.split(':').shift(); 
+    var minHeight = parseFloat(minHeightStr);
+
+    return minArea * minHeight;
+  }
+
+  $scope.maxVolume = function() {
+    var maxAreaStr = $scope.params.area.split(':').pop();
+    var maxArea = parseFloat(maxAreaStr);
+
+    var maxHeightStr = $scope.params.height.split(':').pop(); 
+    var maxHeight = parseFloat(maxHeightStr);
+
+    return maxArea * maxHeight;
+  }
+
+  $scope.areaRangeText = function() {
+    var matches = null;
+    if(matches = $scope.params.area.match(/<(\d+)/)) {
+      return '小于' + matches[1];
+    } else if(matches = $scope.params.area.match(/(\d+):(\d+)/)) {
+      return matches[1] + ' - ' + matches[2];
+    } else if(matches = $scope.params.area.match(/>(\d+)/)) {
+      return '大于' + matches[1];
+    }
+
+    return $scope.params.area;
+  }
+
+  $scope.heightRangeText = function() {
+    var matches = null;
+    if(matches = $scope.params.height.match(/([\d.]+):([\d\.]+)/)) {
+      return matches[1] + ' - ' + matches[2];
+    }
+    
+    return $scope.params.height;
+  }
+
+  $scope.cycleText = function() {
+    if($scope.params.cycle == 5) {
+      return '优秀';
+    } else if($scope.params.cycle == 4) {
+      return '良好';
+    } else if($scope.params.cycle == 3) {
+      return '一般';
+    }
+  }
+
 });
--- app/scripts/controllers/product.js
+++ app/scripts/controllers/product.js
@@ -1,5 +1,28 @@
 'use strict';
 
 angular.module('airCleanersWebApp')
-.controller('ProductCtrl', function ($scope, $http) {
+.controller('ProductCtrl', function ($scope, $http, $routeParams, $filter) {
+
+  $http.get('/data/product.json').success(function(product) {
+    $scope.product = product;
+  });
+
+  $scope.cyclesCount = function() {
+    if(angular.isDefined($scope.product) &&
+        angular.isDefined($scope.product.cadr_dust)) {
+      var cadr = $scope.product.cadr_dust;
+      var maxCount = cadr / $scope.minVolume();
+      var minCount = cadr / $scope.maxVolume();
+
+      return '' + $filter('number')(minCount, 1) + ' - ' +
+        $filter('number')(maxCount, 1);
+    }
+
+    return 'null';
+  };
+
+  $scope.conclusion = function() {
+    return '===结论===';
+  }
+
 });
