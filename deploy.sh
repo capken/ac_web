53c9e8e85687a61c82ae388ab8dbdd3f0f2122c7
Allen Zheng <capken@gmail.com>
2014-10-23 14:44:09 +0800

    add the auto deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,5 @@
 grunt build
 tar -zcvf  public.tar.gz web/public
-scp public.tar.gz mapclipper.com:/home/allen/codes/ac_web/web/
+scp public.tar.gz mapclipper.com:/home/allen/codes/ac_web/
+ssh mapclipper.com "cd /home/allen/codes/ac_web/ && tar -xvf public.tar.gz"
+rm public.tar.gz 
